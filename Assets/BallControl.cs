62d68b724575b207d4580f4615573717f07b71e0
Levi Schaut <schaut.levi@gmail.com>
2023-05-22 17:42:37 -0500

    Update BallControl.cs
    Tweaks the collision math with the ball to fix a bug that can happen in some rare edge cases.

--- Assets/BallControl.cs
+++ Assets/BallControl.cs
@@ -22,14 +22,16 @@ public class BallControl : MonoBehaviour
     }
 
     // Reset the position and velocity of the ball before launching it again.
-    void ResetBall() {
+    void ResetBall() 
+    {
         rb.velocity = new Vector2(0f, 0f);
         transform.position = new Vector2(0f, 0f);
         Invoke("LaunchBall", launchDelay);
     }
 
     // Launch the ball in a random direction.
-    void LaunchBall() {
+    void LaunchBall() 
+    {
         float randNum = Random.Range(0f, 1f);
         if (randNum <= 0.5) {
             rb.AddForce(new Vector2(-ballSpeed, 10));
@@ -39,13 +41,28 @@ public class BallControl : MonoBehaviour
     }
 
     // Update is called once per frame
-    void OnCollisionEnter2D(Collision2D collision)
+    void OnCollisionExit2D(Collision2D collision)
     {
-        if (collision.collider.tag == "Player") {
+        if (collision.collider.tag == "Player") 
+        {
+            // Capture the initial velocities of both objects
             Vector2 ballVel = rb.velocity;
             Vector2 playVel = collision.collider.attachedRigidbody.velocity;
-            rb.velocity = new Vector2(ballVel.x, ballVel.y / 2 + playVel.y / 2);
 
+            // Correct the y-velocity of the ball
+            ballVel.y = ballVel.y / 2 + playVel.y / 2;
+
+            // Correct the x-velocity, if needed
+            if (ballVel.x < 18 && ballVel.x > 0) {
+                ballVel.x = 20;
+            } else if (ballVel.x > -18 && ballVel.x < 0) {
+                ballVel.x = -20;
+            }
+
+            // Set the ball's new velocity to the corrected velocity
+            rb.velocity = ballVel;
+
+            // Play the "click" sound effect with a random pitch
             click.pitch = Random.Range(0.9f, 1.1f);
             click.Play();
         }
