091f89d65192999e8aa37e980bc774409a600412
Levi Schaut <schaut.levi@gmail.com>
2023-05-22 17:41:52 -0500

    Add Reset Button
    Adds a reset button, including its sprites and corresponding code.

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public GUISkin skin;
+    public Transform ball;
 
     private static int playerScore01 = 0;
     private static int playerScore02 = 0;
@@ -17,13 +18,17 @@ public class GameManager : MonoBehaviour
         } else if (wallName == "Left Wall") {
             playerScore02++;
         }
-        Debug.Log("Player score 01: " + playerScore01);
-        Debug.Log("Player score 02: " + playerScore02);
     }
 
     private void OnGUI() {
         GUI.skin = skin;
-        GUI.Label(new Rect(Screen.width / 2 - 150 - 12, 20, 100, 100), playerScore01.ToString());
-        GUI.Label(new Rect(Screen.width / 2 + 150 - 12, 20, 100, 100), playerScore02.ToString());
+        GUI.Label(new Rect(Screen.width / 2 - 150 - 18, 20, 100, 100), playerScore01.ToString());
+        GUI.Label(new Rect(Screen.width / 2 + 150 - 18, 20, 100, 100), playerScore02.ToString());
+
+        if (GUI.Button(new Rect(Screen.width/2-121/2, 35, 121, 53), "Reset")) {
+            playerScore01 = 0;
+            playerScore02 = 0;
+            ball.SendMessage("ResetBall");
+        }
     }
 }
