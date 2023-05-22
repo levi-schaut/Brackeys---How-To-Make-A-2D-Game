504486257f5508a90dc414e10cc4c98c9a41dd8d
Levi Schaut <schaut.levi@gmail.com>
2023-05-21 21:48:16 -0500

    Add Scoring
    Adds several scripts to allow the game to keep score.  Also adds a new font file and GUI skin to handle how the score is displayed.

---
+++ Assets/GameManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GUISkin skin;
+
+    private static int playerScore01 = 0;
+    private static int playerScore02 = 0;
+
+    // Update is called once per frame
+    public static void Score(string wallName)
+    {
+        if (wallName == "Right Wall") {
+            playerScore01++;
+        } else if (wallName == "Left Wall") {
+            playerScore02++;
+        }
+        Debug.Log("Player score 01: " + playerScore01);
+        Debug.Log("Player score 02: " + playerScore02);
+    }
+
+    private void OnGUI() {
+        GUI.skin = skin;
+        GUI.Label(new Rect(Screen.width / 2 - 150, 20, 100, 100), playerScore01.ToString());
+        GUI.Label(new Rect(Screen.width / 2 + 150, 20, 100, 100), playerScore02.ToString());
+    }
+}
---
+++ Assets/SideWalls.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SideWalls : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.name == "Ball") {
+            string wallName = transform.name;
+            GameManager.Score(wallName);
+        }
+    }
+}
