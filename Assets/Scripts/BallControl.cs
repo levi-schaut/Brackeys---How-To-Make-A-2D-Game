83351ebb6f4ec887e6f43d6ed57f406d4720781c
Levi Schaut <schaut.levi@gmail.com>
2023-05-23 17:46:09 -0500

    Modernize Project
    Adds more modern Unity components and adds a few optimizations.

--- Assets/Scripts/BallControl.cs
+++ Assets/Scripts/BallControl.cs
@@ -7,45 +7,41 @@ public class BallControl : MonoBehaviour
     public float ballSpeed = 100f;
     public float launchDelay = 1f;
 
-    private Rigidbody2D rb;
-    private AudioSource click;
-
-    //public enum LaunchDirection
-    //{
-    //    Left,
-    //    Right,
-    //    Random
-    //}
+    public Rigidbody2D ballRigidbody;
+    public AudioSource clickSound;
+    private bool ballIsLaunching;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
-        click = GetComponent<AudioSource>();
-
         // Launches the ball after 2 seconds
         StartCoroutine(WaitToLaunchBall(launchDelay + 1f));
     }
 
     IEnumerator WaitToLaunchBall(float waitTime, string direction = "Random")
     {
+        ballIsLaunching = true;
+
         yield return new WaitForSeconds(waitTime);
         LaunchBall(direction);
+
+        ballIsLaunching = false;
     }
 
     // Reset the position and velocity of the ball before launching it again.
-    void ResetBall(string direction = "Random") 
+    public void ResetBall(string direction = "Random") 
     {
-        rb.velocity = new Vector2(0f, 0f);
-        transform.position = new Vector2(0f, 0f);
-        StartCoroutine(WaitToLaunchBall(launchDelay, direction));
+        if (!ballIsLaunching) {
+            ballRigidbody.velocity = new Vector2(0f, 0f);
+            transform.position = new Vector2(0f, 0f);
+            StartCoroutine(WaitToLaunchBall(launchDelay, direction));
+        }
     }
 
-    // Launch the ball in a random direction.
+    // Launch the ball in a given or random direction.
     void LaunchBall(string direction = "Random") 
     {
-        Debug.Log("Launch Direction = " + direction);
         if (direction == "Random") {
             if (Random.Range(0f, 1f) <= 0.5) {
                 direction = "Left";
@@ -56,19 +52,19 @@ public class BallControl : MonoBehaviour
         }
         float launchAngle = Random.Range(-20f, 20f);
         if (direction == "Left") {
-            rb.AddForce(new Vector2(-ballSpeed, launchAngle));
+            ballRigidbody.AddForce(new Vector2(-ballSpeed, launchAngle));
         } else {
-            rb.AddForce(new Vector2(ballSpeed, launchAngle));
+            ballRigidbody.AddForce(new Vector2(ballSpeed, launchAngle));
         }
     }
 
     // Update is called once per frame
-    void OnCollisionExit2D(Collision2D collision)
+    void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.tag == "Player") 
         {
             // Capture the initial velocities of both objects
-            Vector2 ballVel = rb.velocity;
+            Vector2 ballVel = ballRigidbody.velocity;
             Vector2 playVel = collision.collider.attachedRigidbody.velocity;
 
             // Correct the y-velocity of the ball
@@ -82,11 +78,11 @@ public class BallControl : MonoBehaviour
             }
 
             // Set the ball's new velocity to the corrected velocity
-            rb.velocity = ballVel;
+            ballRigidbody.velocity = ballVel;
 
             // Play the "click" sound effect with a random pitch
-            click.pitch = Random.Range(0.9f, 1.1f);
-            click.Play();
+            clickSound.pitch = Random.Range(0.9f, 1.1f);
+            clickSound.Play();
         }
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,34 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    public GUISkin skin;
     public Transform ball;
+    public Text leftScoreText;
+    public Text rightScoreText;
 
-    private static int playerScore01 = 0;
-    private static int playerScore02 = 0;
+    public static int playerScore01 = 0;
+    public static int playerScore02 = 0;
 
     // Update is called once per frame
-    public static void Score(string wallName)
+    private void Update()
     {
-        if (wallName == "Right Wall") {
+        leftScoreText.text = playerScore01.ToString();
+        rightScoreText.text = playerScore02.ToString();
+    }
+
+    public static void Score(string wallTag)
+    {
+        if (wallTag == "RightWall") {
             playerScore01++;
-        } else if (wallName == "Left Wall") {
+        } else if (wallTag == "LeftWall") {
             playerScore02++;
         }
     }
 
-    private void OnGUI() {
-        GUI.skin = skin;
-        GUI.Label(new Rect(Screen.width / 2 - 150 - 18, 20, 100, 100), playerScore01.ToString());
-        GUI.Label(new Rect(Screen.width / 2 + 150 - 18, 20, 100, 100), playerScore02.ToString());
-
-        if (GUI.Button(new Rect(Screen.width/2-121/2, 35, 121, 53), "Reset")) {
-            playerScore01 = 0;
-            playerScore02 = 0;
-            ball.SendMessage("ResetBall");
-        }
+    public void ResetScore()
+    {
+        playerScore01 = 0;
+        playerScore02 = 0;
     }
 }
